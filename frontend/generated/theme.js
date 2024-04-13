8c88a7ed37932ea0a7840dfaec2ed2aa979bd743
anushri26 <97968818+anushri26@users.noreply.github.com>
2024-04-13 16:42:20 +0530

    Update theme.js

--- frontend/generated/theme.js
+++ frontend/generated/theme.js
@@ -1,2 +1,5 @@
+// Import the applyTheme function from the theme-inventorymanagement.generated.js file
+// and rename it as _applyTheme using the 'as' keyword
 import {applyTheme as _applyTheme} from './theme-inventorymanagement.generated.js';
+// Re-export the applyTheme function under the same name for use in other modules
 export const applyTheme = _applyTheme;
