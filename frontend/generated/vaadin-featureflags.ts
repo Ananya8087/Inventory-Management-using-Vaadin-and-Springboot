444b4020db5544b151e4ddb85c171697a66257ab
anushri26 <97968818+anushri26@users.noreply.github.com>
2024-04-13 16:48:41 +0530

    Update vaadin-featureflags.ts

--- frontend/generated/vaadin-featureflags.ts
+++ frontend/generated/vaadin-featureflags.ts
@@ -1,6 +1,9 @@
 // @ts-nocheck
+// Initialize the Vaadin object 
 window.Vaadin = window.Vaadin || {};
+// Initialize the featureFlags object under the Vaadin namespace 
 window.Vaadin.featureFlags = window.Vaadin.featureFlags || {};
+// Set exampleFeatureFlag to false
 window.Vaadin.featureFlags.exampleFeatureFlag = false;
 window.Vaadin.featureFlags.collaborationEngineBackend = false;
 export {};
