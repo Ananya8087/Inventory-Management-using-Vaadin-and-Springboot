ccc0ebfd0e01c56f6b5ecef4632e2455c88ee679
anushri26 <97968818+anushri26@users.noreply.github.com>
2024-04-13 16:45:13 +0530

    Update vaadin.ts

--- frontend/generated/vaadin.ts
+++ frontend/generated/vaadin.ts
@@ -1,7 +1,8 @@
+
 import './vaadin-featureflags.ts';
 
 import './index';
-
+// This file may contain development tools or utilities provided by the Vaadin framework
 import 'Frontend/generated/jar-resources/vaadin-dev-tools/vaadin-dev-tools.js';
 
 import { applyTheme } from './theme.js';
