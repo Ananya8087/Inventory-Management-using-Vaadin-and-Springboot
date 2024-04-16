171a16fee449af6142e13cc2f2d0cc3119edc313
Ananya V <90900377+Ananya8087@users.noreply.github.com>
2024-04-16 13:14:36 +0530

    Update DatabaseUtil.java

--- src/main/java/com/example/application/views/util/DatabaseUtil.java
+++ src/main/java/com/example/application/views/util/DatabaseUtil.java
@@ -6,9 +6,9 @@ import java.sql.DriverManager;
 
 public class DatabaseUtil {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/ooad";
+    private static final String URL = "jdbc:mysql://localhost:3306/yourdatabase";
     private static final String USER = "root";
-    private static final String PASSWORD = "ananya123";
+    private static final String PASSWORD = "password";
 
     public static Connection getConnection() {
         try {
